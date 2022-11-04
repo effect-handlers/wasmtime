4f13cbb5191d450851eaa9df060ece0297750fc5
cosine <trash@cosine.online>
2022-11-04 14:47:36 -0400

    Set stack limit to zero (hack); Remove prints/panics

--- crates/runtime/src/libcalls.rs
+++ crates/runtime/src/libcalls.rs
@@ -352,16 +352,12 @@ unsafe fn cont_new(vmctx: *mut VMContext, func: *mut u8) -> *mut u8 {
         Fiber::new(
             FiberStack::new(4096).unwrap(),
             move |first_resumption: (), suspend: &Suspend<_, (), _>| {
-                eprintln!("resuming");
-                eprintln!("func ptr is {:?}", (*func).func_ptr.as_ptr());
                 let trampoline = mem::transmute::<
                     *const VMFunctionBody,
                     unsafe extern "C" fn(*mut VMOpaqueContext, *mut VMContext),
                 >((*func).func_ptr.as_ptr());
                 let trampoline = prepare_host_to_wasm_trampoline(vmctx, trampoline);
-                eprintln!("have trampoline {:?}", trampoline);
                 trampoline((*func).vmctx, vmctx);
-                eprintln!("done with trampoline! uh oh");
             },
         )
         .unwrap(),
@@ -374,13 +370,12 @@ unsafe fn cont_new(vmctx: *mut VMContext, func: *mut u8) -> *mut u8 {
 unsafe fn resume(vmctx: *mut VMContext, cont: *mut u8) {
     let inst = vmctx.as_mut().unwrap().instance_mut();
     let cont = cont as *mut Fiber<'static, (), (), ()>;
-    eprintln!(
-        "resume setting tsp to {:?}",
-        cont.as_ref().unwrap().stack.top().unwrap()
-    );
     let cont_stack = &cont.as_ref().unwrap().stack;
     cont_stack.write_parent(inst.tsp());
     inst.set_tsp(cont_stack.top().unwrap());
+    (*(*(*(*vmctx).instance().store()).vmruntime_limits())
+        .stack_limit
+        .get_mut()) = 0;
     match cont.as_mut().unwrap().resume(()) {
         Ok(res) => (),
         Err(y) => (),
@@ -390,7 +385,6 @@ unsafe fn resume(vmctx: *mut VMContext, cont: *mut u8) {
 
 // Implementation of `suspend`
 unsafe fn suspend(vmctx: *mut VMContext) {
-    panic!("suspending");
     let inst = vmctx.as_mut().unwrap().instance_mut();
     let stack_ptr = inst.tsp();
     let parent = stack_ptr.cast::<*mut u8>().offset(-2).read();
