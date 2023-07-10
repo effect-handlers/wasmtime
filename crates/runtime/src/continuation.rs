18a2dd167a1b1baa52c88012f1602844eca6ced2
Frank Emrich <git@emrich.io>
2023-07-10 15:37:52 +0100

    dedicated function for advancing within Payloads object

--- crates/runtime/src/continuation.rs
+++ crates/runtime/src/continuation.rs
@@ -26,6 +26,13 @@ impl Payloads {
             data: ptr::null_mut(),
         };
     }
+
+    fn occupy_next(&mut self, count: usize) -> *mut u128 {
+        let original_length = self.length;
+        assert!(self.length + count <= self.capacity);
+        self.length += count;
+        return unsafe {self.data.offset(original_length as isize)};
+    }
 }
 
 /// Encodes the life cycle of a `ContinuationObject`.
@@ -102,10 +109,8 @@ pub fn cont_obj_occupy_next_args_slots(
     arg_count: usize,
 ) -> *mut u128 {
     assert!(unsafe { (*obj).state == State::Allocated });
-    let args_len = unsafe { (*obj).args.length };
-    unsafe { (*obj).args.length += arg_count };
-    assert!(unsafe { (*obj).args.length <= (*obj).args.capacity });
-    unsafe { (*obj).args.data.offset(args_len as isize) }
+    let args = &mut unsafe {obj.as_mut()}.unwrap().args;
+    return args.occupy_next(arg_count);
 }
 
 /// TODO
